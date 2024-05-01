5f99116e48928b26b4603b1b03e83bd775828f91
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2024-05-01 15:56:23 +0200

    artifex-rpc: make crate wasm32-compatible
    If this crate is used as a dependency by another crate built for wasm32
    target, tonic should not be built with its default features.
    
    So update Cargo.toml accordingly.

--- artifex-rpc/build.rs
+++ artifex-rpc/build.rs
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2022 Eric Le Bihan <eric.le.bihan.dev@free.fr>
+// Copyright (C) 2022-2024 Eric Le Bihan <eric.le.bihan.dev@free.fr>
 //
 // SPDX-License-Identifier: MIT
 //
@@ -7,11 +7,18 @@
 use std::{env, error::Error, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    tonic_build::configure()
-        .file_descriptor_set_path(out_dir.join("artifex_descriptor.bin"))
-        .compile(&["proto/artifex.proto"], &["proto"])
-        .unwrap();
-    tonic_build::compile_protos("proto/artifex.proto")?;
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH")?;
+    let builder = match target_arch.as_str() {
+        "wasm32" => tonic_build::configure()
+            .build_server(false)
+            .build_client(true)
+            .build_transport(false),
+        _ => {
+            let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+            tonic_build::configure()
+                .file_descriptor_set_path(out_dir.join("artifex_descriptor.bin"))
+        }
+    };
+    builder.compile(&["proto/artifex.proto"], &["proto"])?;
     Ok(())
 }
--- artifex-rpc/src/lib.rs
+++ artifex-rpc/src/lib.rs
@@ -6,4 +6,5 @@
 
 tonic::include_proto!("artifex");
 
+#[cfg(not(target_arch = "wasm32"))]
 pub const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("artifex_descriptor");

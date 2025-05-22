0916e5d094c47e3598a77b49a216274a1c04f8b8
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2025-05-22 19:49:16 +0200

    client-cli: remove impl Default for tls::Config
    In order to be able to reuse tls::Config in other clients, do not implement
    Default for tls::Config and let the program-level Config set the proper default
    values for TLS.

--- artifex-client-cli/src/tls.rs
+++ artifex-client-cli/src/tls.rs
@@ -48,27 +48,6 @@ pub struct Config {
     pub server_alt_name: Option<String>,
 }
 
-impl Default for Config {
-    fn default() -> Self {
-        Self {
-            root_cert: PathBuf::from(Self::DEFAULT_ROOT_CERT),
-            client_cert: PathBuf::from(Self::DEFAULT_CLIENT_CERT),
-            client_key: PathBuf::from(Self::DEFAULT_CLIENT_KEY),
-            client_password: None,
-            server_alt_name: None,
-        }
-    }
-}
-
-impl Config {
-    /// Default root certification authoritity file.
-    pub const DEFAULT_ROOT_CERT: &str = "/etc/artifex/root.crt.pem";
-    /// Default client certificate.
-    pub const DEFAULT_CLIENT_CERT: &str = "/etc/artifex/client.crt.pem";
-    /// Default client private key.
-    pub const DEFAULT_CLIENT_KEY: &str = "/etc/artifex/client.key.pem";
-}
-
 /// Create TLS client configuration.
 pub fn create_client_config(config: &Config) -> Result<ClientConfig, Error> {
     let mut ca_store = RootCertStore::empty();
